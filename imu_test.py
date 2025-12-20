f3d454c7137f0d263ec3b5b6fd008a113c7946a2
carlin <marycarl@ualberta.ca>
2025-12-20 15:48:43 -0700

    Comments

--- imu_test.py
+++ imu_test.py
@@ -1,11 +1,15 @@
 import smbus, time
+"""
+Test IMU is powered
+Read WHO_AM_I register
+"""
 
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(1) # Open I2C bus #1
 addr = 0x6A
 
 while True:
     try:
-        who = bus.read_byte_data(addr, 0x0F)
+        who = bus.read_byte_data(addr, 0x0F) # 0x0F = WHO_AM_I register
         print(f"WHO_AM_I: 0x{who:X}")
     except Exception as e:
         print("I2C error:", e)
